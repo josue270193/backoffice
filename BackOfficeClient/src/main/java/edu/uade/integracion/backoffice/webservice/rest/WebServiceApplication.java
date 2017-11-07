8745b561676fc0ef94e3cfcdb7f98f8c2545eaa4
fgukmar <fgukmar@Equipo006500.corp.cablevision.com.ar>
2017-11-07 09:53:48 -0300

    .

--- BackOfficeClient/src/main/java/edu/uade/integracion/backoffice/webservice/rest/WebServiceApplication.java
+++ BackOfficeClient/src/main/java/edu/uade/integracion/backoffice/webservice/rest/WebServiceApplication.java
@@ -3,6 +3,7 @@ package edu.uade.integracion.backoffice.webservice.rest;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
+//@ApplicationPath("/ServiciosBO")
 @ApplicationPath("/ServiciosBO")
 public class WebServiceApplication extends Application {
 }
